b982db75876f957315d13f5289aa68170be7cdd0
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-07-14 15:53:43 +0530

    Update Data_Sender.hpp

--- Data_Sender.hpp
+++ Data_Sender.hpp
@@ -1,4 +1,4 @@
 #include <stdio.h>
 
-void readSenderDataFromFile(float Temperature, int StateOfCharge, char *inputFileName);
-void sendDataOnConsole(float Temperature, int StateOfCharge);
+void readSenderDataFromFile(float *Temperature, int *StateOfCharge, char *inputFileName);
+void sendDataOnConsole(float *Temperature, int *StateOfCharge);
