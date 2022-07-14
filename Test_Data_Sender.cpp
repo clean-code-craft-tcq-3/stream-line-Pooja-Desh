398ccff33ca1b874a8cb8e1e5051da4839552274
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-07-15 01:51:25 +0530

    Update Test_Data_Sender.cpp

--- Test_Data_Sender.cpp
+++ Test_Data_Sender.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Check if data is read from the file")
 {
 float Temperature[50] = {0};
 int SOC[50] = {0};
-float expectedOutput[3][2] = {{10.2,1},{32.4,2},{25.1,3}};
+float expectedOutput[3][2] = {{10,1},{32,2},{25,3}};
 FILE* fptr = fopen("./SensorData.txt","r");
 readSenderDataFromFile(Temperature, SOC, "./SensorData.txt");
 fclose(fptr);
