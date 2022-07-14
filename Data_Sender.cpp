45bff6876a3756acbb75c1bdc490acfbe10b348f
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-07-14 13:35:32 +0530

    Create Data_Sender.cpp

---
+++ Data_Sender.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <Data_Sender.hpp>
+
+void readSenderDataFromFile(float Temperature, int StateOfCharge,char *fileName)
+{
+  FILE *senderDataFile = fopen(filename,"r");
+}
+
+void sendDataOnConsole(float Temperature, int StateOfCharge)
+{
+  
+}
