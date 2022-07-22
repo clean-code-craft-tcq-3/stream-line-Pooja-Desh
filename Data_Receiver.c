5e859ef92fef7ada67f12cb883a0d19714da9c69
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-22 16:25:03 +0530

    Update Data_Receiver.c

--- Data_Receiver.c
+++ Data_Receiver.c
@@ -1,1 +1,11 @@
-
+void readDataFromConsole(char consoleoutput[count][10])
+{
+  char data[count];
+  char *output;
+  output = fgets(data,count,stdin);
+  for (int index=0;output!=NULL;index++)
+  {
+    output = fgets(data,count,stdin);
+    strcpy(consoleoutput[index],data);
+  }
+}
