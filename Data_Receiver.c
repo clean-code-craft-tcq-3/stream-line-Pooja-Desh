5bfccd61aa89c465c55aab9912e78cac252743ee
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-27 15:46:45 +0530

    Update Data_Receiver.c

--- Data_Receiver.c
+++ Data_Receiver.c
@@ -1,18 +1,13 @@
 #include "Data_Receiver.h"
 
-void readDataFromConsole(char consoleoutput[count][50])
+void readDataFromConsole(float* Temperature, float* SOC)
 {
-  char data[count];
-  char *output;
-  output = fgets(data,count,stdin);
-  for (int index=0;output!=NULL;index++)
+  for(int index = 0; index < readings_count; index++)
   {
-    scanf("%s",data);
-    output = data;
-    strcpy(consoleoutput[index],data);
-    printf("%s",consoleoutput);
+    scanf("%f ,%f ",&Temperature[index],&SOC[index]);
   }
 }
+  
 
 int main()
 {
