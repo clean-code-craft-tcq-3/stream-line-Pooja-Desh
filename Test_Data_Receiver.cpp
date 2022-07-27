c54be3f27d675bc05b57e269932a12841f0c6ebf
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-27 19:32:38 +0530

    Update Test_Data_Receiver.cpp

--- Test_Data_Receiver.cpp
+++ Test_Data_Receiver.cpp
@@ -6,7 +6,7 @@
 TEST_CASE("Tests to check whether sensor data is read from console") 
 {
   float Temperature[readings_count] = {0};
-  float SOC[readings_count] = {0};
+  int SOC[readings_count] = {0};
   //float observedMaxValue, observedMinValue, observedSMAValue, expectedMaxValue, expectedMinValue, expectedSMAValue;
   receiveAndProcessSensorData(&Temperature[0],&SOC[0]);
   //float expectedoutput[2][2] = {{10,1}, {20.2,2}};
