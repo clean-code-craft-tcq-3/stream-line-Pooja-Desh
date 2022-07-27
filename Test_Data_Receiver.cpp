79669a7e65de35c9d23773f055305b7b8a6c8c75
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-27 16:36:21 +0530

    Update Test_Data_Receiver.cpp

--- Test_Data_Receiver.cpp
+++ Test_Data_Receiver.cpp
@@ -1,4 +1,9 @@
-#define CATCH_CONFIG_MAIN
-
 #include "test/catch.hpp"
 #include "Data_Receiver.h"
+
+TEST_CASE("Tests to check whether sensor data is read from console") 
+{
+  float Temperature[readings_count] = {0};
+  float SOC[readings_count] = {0};
+  receiveAndProcessSensorData(&Temperature[0],&SOC[0]);
+}
