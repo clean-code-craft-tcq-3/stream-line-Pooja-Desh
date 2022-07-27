e6264cad9831772aa9807fa4abe31b90ceabf03d
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-27 16:24:29 +0530

    Update Data_Receiver.h

--- Data_Receiver.h
+++ Data_Receiver.h
@@ -1,7 +1,12 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
 
 #define readings_count 50
 
 void readDataFromConsole(float* Temperature, float* SOC);
+float getMaxValue(float *sensorparameter);
+float getMinValue(float *sensorparameter);
+float calculateSimpleMovingAverage(float *sensorparameter);
+void printReceivedDataToConsole(float *sensorparameter, float maxvalue, float minvalue, float SMA);
+void receiveAndProcessSensorData(float* Temperature, float* SOC);
+
