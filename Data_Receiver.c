9dcd0c5bfd0921964a27344376ab59ca94ec29c2
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-27 14:27:50 +0530

    Update Data_Receiver.c

--- Data_Receiver.c
+++ Data_Receiver.c
@@ -7,7 +7,8 @@ void readDataFromConsole(char consoleoutput[count][50])
   output = fgets(data,count,stdin);
   for (int index=0;output!=NULL;index++)
   {
-    output = fgets(data,count,stdin);
+    scanf("%s",data);
+    output = data;
     strcpy(consoleoutput[index],data);
   }
 }
