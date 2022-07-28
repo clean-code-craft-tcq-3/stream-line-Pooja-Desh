edcaca62cf3053e1fd41e6ab1573288906a692e5
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-28 13:07:22 +0530

    Update Data_Sender.cpp

--- Data_Sender.cpp
+++ Data_Sender.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "Data_Sender.hpp"
 
-void readSenderDataFromFile(float *Temperature, int *StateOfCharge,char *inputFileName)
+void readSenderDataFromFile(float *Temperature, float *StateOfCharge,char *inputFileName)
 {
   float TemperatureData;
   int StateOfChargeData;
@@ -10,7 +10,7 @@ void readSenderDataFromFile(float *Temperature, int *StateOfCharge,char *inputFi
   FILE *SensorDataFile = fopen(inputFileName,"r");
   if (SensorDataFile != NULL)
   {
-    for(int FileIndex = 0; fscanf(SensorDataFile,"%f %d\n", &TemperatureData, &StateOfChargeData)!=EOF; FileIndex++)
+    for(int FileIndex = 0; fscanf(SensorDataFile,"%f %f\n", &TemperatureData, &StateOfChargeData)!=EOF; FileIndex++)
     {
       Temperature[FileIndex] = TemperatureData;
       StateOfCharge[FileIndex] = StateOfChargeData;
@@ -19,11 +19,11 @@ void readSenderDataFromFile(float *Temperature, int *StateOfCharge,char *inputFi
   fclose(SensorDataFile);
 }
 
-void sendDataOnConsole(float *Temperature, int *StateOfCharge)
+void sendDataOnConsole(float *Temperature, float *StateOfCharge)
 {
   //printf("Temperature  StateOfCharge\n");
   for(int ReadingIndex=0; ReadingIndex < 50; ReadingIndex++)
   {
-    printf("%.2f\t %d\n",Temperature[ReadingIndex],StateOfCharge[ReadingIndex]);
+    printf("%.2f\t %.2f\n",Temperature[ReadingIndex],StateOfCharge[ReadingIndex]);
   }
 }
