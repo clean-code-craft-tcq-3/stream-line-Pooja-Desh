140643b1f59afbef82ea00fbf05d1dc12d7ab051
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-29 14:20:12 +0530

    Update Data_Receiver.h

--- Data_Receiver.h
+++ Data_Receiver.h
@@ -8,6 +8,6 @@ void readDataFromConsole(float* Temperature, float* SOC);
 float getMaxValue(float *sensorparameter);
 float getMinValue(float *sensorparameter);
 float calculateSimpleMovingAverage(float *sensorparameter);
-void printReceivedDataToConsole(float *sensorparameter, float maxvalue, float minvalue, float SMA);
+int printReceivedDataToConsole(float *sensorparameter, float maxvalue, float minvalue, float SMA);
 void receiveAndProcessSensorData(float* Temperature, float* SOC);
 
